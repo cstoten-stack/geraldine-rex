93a1e379d4a8808d189c265ffd4189ec3f1d6fb3
cstoten-stack <c.stoten@gmail.com>
2025-12-29 13:52:32 +0000

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,20 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value:
+              "frame-ancestors https://*.rexsoftware.com https://*.rexsoftware.io;",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
